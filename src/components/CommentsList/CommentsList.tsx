cb958c1f066ecc6d309844ba8c7eeb9ae59c67d1
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-12-23 23:56:27 +0200

    Fix component Post and CommentList

--- src/components/CommentsList/CommentsList.tsx
+++ src/components/CommentsList/CommentsList.tsx
@@ -3,7 +3,7 @@ import { List, ListItem, ListItemText, Typography } from "@mui/material"
 export const CommentsList = () => {
 
     return (
-        <List sx={{maxHeight: '100px', overflow: 'auto', scrollbarWidth: 'thin', scrollbarColor: 'gray transparent'}}>
+        <List sx={{backgroundColor: '#fcfcfc', maxHeight: '100px', overflow: 'auto', scrollbarWidth: 'thin', scrollbarColor: 'gray transparent'}}>
             <ListItem alignItems="flex-start" sx={{padding: '4px 16px'}}>
                 <ListItemText>
                     <Typography variant='body2' fontWeight='500'>and.yevchenko</Typography>
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -7,28 +7,22 @@ import { User } from '../User/User';
 import './Post.css';
 
 export const Post = () => {
-
-
-    //TODO
+//TODO
     return (
         <div className='post'>
-
             <div className='post-head'>
                 <User />
                 <MoreOptions />
             </div>
-
             <div className='post-main'>
                 <PostText />
                 <img className='post-img' src="/witcher.jpg" alt="witcher" width="100%"/>
             </div>
-
             <div className='post-foot'>
                 <PostPanel />
                 <CommentsList />
                 <AddComment />
             </div>
-
         </div>
     )
 }
