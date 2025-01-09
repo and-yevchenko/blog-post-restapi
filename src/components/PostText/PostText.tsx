591fc5087536d4039a63187968a2547241c85920
Andrii Yevchenko <and.yevchenko@gmail.com>
2025-01-09 15:37:51 +0200

    Fix func read more text by PostText and fix styles

--- src/components/PostText/PostText.tsx
+++ src/components/PostText/PostText.tsx
@@ -15,17 +15,19 @@ export const PostText: React.FC<PostTextProps> = ({ text, maxLength = 100 }) => 
 
     return (
         <Box sx={{ padding: '1rem' }} justifyItems="start">
-            <Typography variant="body1" textAlign="start">
-                {isExpanded ? text : `${text.substring(0, maxLength)}...`}
+            <Typography variant="body1" textAlign="start" sx={{textWrap: 'wrap', wordBreak: 'break-word'}}>
+                {text.length >= maxLength ? `${isExpanded ? text : `${text.substring(0, maxLength)}...`}` : text}
             </Typography>
-            <Button
-                variant="text"
-                color="inherit"
-                size="small"
-                onClick={onReadMore}
-            >
-                {isExpanded ? 'Read less' : 'Read more'}
-            </Button>
+            {text.length >= maxLength && 
+                <Button
+                    variant="text"
+                    color="inherit"
+                    size="small"
+                    onClick={onReadMore}
+                >
+                    {isExpanded ? 'Read less' : 'Read more'}
+                </Button>
+            }
         </Box>
     );
 };
